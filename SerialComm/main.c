cf2d6d1a9d147c726b8522f555850131de7a1739
unknown <cookab1@CS-317-9K6NFQ1.appd.appstate.edu>
2018-02-13 14:43:44 -0500

    init commit

---
+++ SerialComm/main.c
@@ -0,0 +1,18 @@
+/*
+ * SerialComm.c
+ *
+ * Created: 2/13/2018 2:41:06 PM
+ * Author : cookab1
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
