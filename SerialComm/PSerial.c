9924cff28acd003116f4ca2b039e3c88c14669ab
Andy Cook <andyrock.cook@gmail.com>
2018-02-14 21:46:14 -0500

    First work on read and write

--- SerialComm/PSerial.c
+++ SerialComm/PSerial.c
@@ -28,15 +28,31 @@ SERIAL_REGS * serial_port[] = {
 };
 
 void PSerial_open(unsigned int portNum, long speed, int config) {
-	port = portNum;
+	port = portNum; //why was this a character?
 	baudrate = speed;
 	framing = config;
 }
+/*
+UCSRnA 6 is Transmit Complete and 7 is Receive Complete
+UCSRnB 3 is Transmit Enable and 4 is Receive Enable
+*/
 char PSerial_read(unsigned char port) {
-	
+	//enable the receiving 
+	//UCSRnB bit 4
+	UCSR0B |= 1 << 4;
+	char c = 0;
+	while(!(UCSR0A & 0x80)) {
+		//c = read in the data
+	}
+	return c;
 }
 void PSerial_write(unsigned char port, char data) {
-	
+	//enable the transmitting
+	//UCSRnB bit 3
+	UCSR0B |= 1 << 3;
+	while(!(UCSR0A & 0x40)) {
+		//write in the data
+	}
 }
 
 
